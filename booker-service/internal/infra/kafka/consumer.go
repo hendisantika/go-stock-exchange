04c0edb9221358a49e49fd931ac5e326f2bc6c2f
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:30:16 +0700

    Add Consume🫸🌀✏️📗🐧🐳⬆

--- booker-service/internal/infra/kafka/consumer.go
+++ booker-service/internal/infra/kafka/consumer.go
@@ -13,3 +13,22 @@ func NewConsumer(configMap *ckafka.ConfigMap, topics []string) *Consumer {
 		Topics:    topics,
 	}
 }
+
+func (c *Consumer) Consume(msgChan chan *ckafka.Message) {
+	consumer, err := ckafka.NewConsumer(c.ConfigMap)
+	if err != nil {
+		panic(err)
+	}
+
+	err = consumer.SubscribeTopics(c.Topics, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err == nil {
+			msgChan <- msg
+		}
+	}
+}
