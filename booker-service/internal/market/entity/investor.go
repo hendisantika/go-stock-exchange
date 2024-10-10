a641a87c33b13ee03586d8638bfc700c0818db4a
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:33:58 +0700

    Add NewInvestor struct🫸🌀✏️📗🐧🐳⬆

--- booker-service/internal/market/entity/investor.go
+++ booker-service/internal/market/entity/investor.go
@@ -5,3 +5,10 @@ type Investor struct {
 	Name          string
 	AssetPosition []*InvestorAssetPosition
 }
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
