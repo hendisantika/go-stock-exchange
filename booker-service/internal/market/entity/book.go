e99db181868ab809e38e12102080512e8b99d8b0
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:32:34 +0700

    Add Trade struct🫸🌀✏️📗🐧🐳⬆

--- booker-service/internal/market/entity/book.go
+++ booker-service/internal/market/entity/book.go
@@ -1,6 +1,9 @@
 package entity
 
-import "sync"
+import (
+	"container/heap"
+	"sync"
+)
 
 type Book struct {
 	Order         []*Order
@@ -17,5 +20,61 @@ func NewBook(orderChannel chan *Order, orderChanOut chan *Order, wg *sync.WaitGr
 		OrdersChan:    orderChannel,
 		OrdersChanOut: orderChanOut,
 		Wg:            wg,
+	}
+}
+
+func (b *Book) Trade() {
+	buyOrders := make(map[string]*OrderQueue)
+	sellOrders := make(map[string]*OrderQueue)
+
+	for order := range b.OrdersChan {
+
+		asset := order.Asset.ID
+
+		if buyOrders[asset] == nil {
+			buyOrders[asset] = NewOrderQueue()
+			heap.Init(buyOrders[asset])
+		}
+
+		if sellOrders[asset] == nil {
+			sellOrders[asset] = NewOrderQueue()
+			heap.Init(sellOrders[asset])
+		}
+
+		if order.OrderType == "BUY" {
+			buyOrders[asset].Push(order)
+			if sellOrders[asset].Len() > 0 && sellOrders[asset].Orders[0].Price <= order.Price {
+				sellOrder := sellOrders[asset].Pop().(*Order)
+				if sellOrder.PendingShares > 0 {
+					transaction := NewTransaction(sellOrder, order, order.Shares, sellOrder.Price)
+					b.AddTransaction(transaction, b.Wg)
+					sellOrder.Transactions = append(sellOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+					b.OrdersChanOut <- sellOrder
+					b.OrdersChanOut <- order
+
+					if sellOrder.PendingShares > 0 {
+						sellOrders[asset].Push(sellOrder)
+					}
+				}
+			}
+		} else if order.OrderType == "SELL" {
+			sellOrders[asset].Push(order)
+			if buyOrders[asset].Len() > 0 && buyOrders[asset].Orders[0].Price >= order.Price {
+				buyOrder := buyOrders[asset].Pop().(*Order)
+				if buyOrder.PendingShares > 0 {
+					transaction := NewTransaction(order, buyOrder, order.Shares, buyOrder.Price)
+					b.AddTransaction(transaction, b.Wg)
+					buyOrder.Transactions = append(buyOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+					b.OrdersChanOut <- buyOrder
+					b.OrdersChanOut <- order
+
+					if buyOrder.PendingShares > 0 {
+						buyOrders[asset].Push(buyOrder)
+					}
+				}
+			}
+		}
 	}
 }
