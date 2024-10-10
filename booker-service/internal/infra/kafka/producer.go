a67c30a942521e6bdff0362a582070a95a860ebc
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:28:44 +0700

    Add NewKafkaProducer🫸🌀✏️📗🐧🐳⬆

--- booker-service/internal/infra/kafka/producer.go
+++ booker-service/internal/infra/kafka/producer.go
@@ -3,3 +3,9 @@ package kafka
 type Producer struct {
 	ConfigMap *ckafka.ConfigMap
 }
+
+func NewKafkaProducer(configMap *ckafka.ConfigMap) *Producer {
+	return &Producer{
+		ConfigMap: configMap,
+	}
+}
