70a4ff219bad58bb177030a1dfa681abd65fe7a0
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:35:14 +0700

    Add Order struct🫸🌀✏️📗🐧🐳⬆

---
+++ booker-service/internal/market/entity/order.go
@@ -0,0 +1,13 @@
+package entity
+
+type Order struct {
+	ID            string
+	Investor      *Investor
+	Asset         *Asset
+	Shares        int
+	PendingShares int
+	Price         float64
+	OrderType     string
+	Status        string
+	Transactions  []*Transaction
+}
