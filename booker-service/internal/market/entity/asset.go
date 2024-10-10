785cbf8a59658f057482301fbe5a1b41b51bd853
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:31:52 +0700

    Add NewAsset struct🫸🌀✏️📗🐧🐳⬆

--- booker-service/internal/market/entity/asset.go
+++ booker-service/internal/market/entity/asset.go
@@ -5,3 +5,11 @@ type Asset struct {
 	Name         string
 	MarketVolume int
 }
+
+func NewAsset(id string, name string, marketVolume int) *Asset {
+	return &Asset{
+		ID:           id,
+		Name:         name,
+		MarketVolume: marketVolume,
+	}
+}
