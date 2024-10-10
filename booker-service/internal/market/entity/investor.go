05ee92e3685084a95c5b86d1ad9ae10b7e1978e3
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:34:52 +0700

    Add NewInvestorAssetPosition func🫸🌀✏️📗🐧🐳⬆

--- booker-service/internal/market/entity/investor.go
+++ booker-service/internal/market/entity/investor.go
@@ -40,3 +40,10 @@ type InvestorAssetPosition struct {
 	AssetID string
 	Shares  int
 }
+
+func NewInvestorAssetPosition(assetID string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetID,
+		Shares:  shares,
+	}
+}
