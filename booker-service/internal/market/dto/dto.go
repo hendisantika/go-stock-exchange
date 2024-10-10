0cc9bbf5e4303febc6f74df7068448dfb38e3e0d
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 06:31:16 +0700

    Add TransactionOutput struct🫸🌀✏️📗🐧🐳⬆

--- booker-service/internal/market/dto/dto.go
+++ booker-service/internal/market/dto/dto.go
@@ -20,3 +20,12 @@ type OrderOutput struct {
 	Shares             int                  `json:"shares"`
 	TransactionsOutput []*TransactionOutput `json:"transaction_output"`
 }
+
+type TransactionOutput struct {
+	TransactionID string  `json:"transaction_id"`
+	BuyerID       string  `json:"buyer_id"`
+	SellerID      string  `json:"seller_id"`
+	AssetID       string  `json:"asset_id"`
+	Price         float64 `json:"price"`
+	Shares        int     `json:"shares"`
+}
