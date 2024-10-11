e86bc81bdb541aaf120c510bf4cd2bab84bf207c
hendisantika <hendisantika@yahoo.co.id>
2024-10-11 07:22:55 +0700

    Create broker-backend🫸🌀✏️📗🐧🐳⬆

--- broker-backend/go.mod
+++
@@ -1,1 +0,0 @@
-module broker-backend
---
+++ broker-backend/src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+import {Global, Module} from '@nestjs/common';
+import {PrismaService} from './prisma/prisma.service';
+
+@Global() //evita ter que importar esse módulo, já que será usado em vários outros módulos
+@Module({
+    providers: [PrismaService],
+    exports: [PrismaService]
+})
+export class PrismaModule {
+}
